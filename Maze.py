0b5dfef293b2a758a45474136660a6edf6293df2
Saltal27 <omarmobarak53@gmail.com>
2022-09-24 16:45:59 +0300

    Update Maze.py

--- Maze.py
+++ Maze.py
@@ -7,6 +7,8 @@ while not at_goal():
     if right_is_clear():
         turn_right()
         move()
+    elif front_is_clear():
+        move()
     else:
         turn_left()
 ################################################################
